066d97c94335109592b5c32351e06d74a645610c
Genki Marshall <genkimarshall@gmail.com>
2014-09-27 14:33:48 -0400

    Testies

--- wsgi/openshift/market/static/js/requests.js
+++ wsgi/openshift/market/static/js/requests.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-	var areas = ["North Campus", "West Campus", "Collegtown"];
-	var filtersContainer = $('#filters');
-	for (i=0; i<areas.length; i++) {
-		filtersContainer.append('<input type="checkbox" value="'+ areas[i] + '"/> ' + areas[i] + '<br />');
-	}
-
-	var sorts = ["Item", "Price", "Location"];
-	var sortContainer = $('#sorts');
-	for (i=0; i<sorts.length; i++) {
-		sortContainer.append('<button type="button" class="btn btn-default"> ' + sorts[i] + '</button>');
-	}
-
-	// var areas = ["Name v", "Price v"];
-	// var checkboxContainer = $('#checkboxes');
-	// for (i=0; i<areas.length; i++) {
-	// 	checkboxContainer.append('<input type="checkbox" value="'+ areas[i] + '"/> ' + areas[i] + '<br />');
-	// }
+	var url = "localhost:8000/api/getjson";
+	var settings = {
+		data: 'category=A',
+	};
+	// $.ajax(url, settings).done(function(result) {
+	// 	var result = JSON.parse(result);
+	// 	console.log(result);
+	// });
+	$.get(
+		"localhost:8000/api/getjson", 
+		{ category: 'A' },
+	    function(result) {
+			var result = JSON.parse(result);
+			console.log(result);
+		}
+	);
 });
