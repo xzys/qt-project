6b76777218411f706376ef92b44a302ffbe1f3b6
adwittumuluri <AdwitTumuluri@dhcp-ccc-438.eduroam.cornell.edu>
2014-09-27 12:31:05 -0400

    nothing

--- environment/lib/python2.7/site-packages/django/contrib/messages/tests/test_cookie.py
+++ environment/lib/python2.7/site-packages/django/contrib/messages/tests/test_cookie.py
@@ -5,7 +5,8 @@ from django.contrib.messages.tests.base import BaseTests
 from django.contrib.messages.storage.cookie import (CookieStorage,
     MessageEncoder, MessageDecoder)
 from django.contrib.messages.storage.base import Message
-from django.test import TestCase, override_settings
+from django.test import TestCase
+from django.test.utils import override_settings
 from django.utils.safestring import SafeData, mark_safe
 
 
@@ -40,7 +41,7 @@ def stored_cookie_messages_count(storage, response):
     return len(data)
 
 
-@override_settings(SESSION_COOKIE_DOMAIN='.example.com', SESSION_COOKIE_SECURE=True, SESSION_COOKIE_HTTPONLY=True)
+@override_settings(SESSION_COOKIE_DOMAIN='.example.com')
 class CookieTest(BaseTests, TestCase):
     storage_class = CookieStorage
 
@@ -55,10 +56,10 @@ class CookieTest(BaseTests, TestCase):
         # Test that the message actually contains what we expect.
         self.assertEqual(list(storage), example_messages)
 
-    def test_cookie_setings(self):
+    def test_domain(self):
         """
-        Ensure that CookieStorage honors SESSION_COOKIE_DOMAIN, SESSION_COOKIE_SECURE and SESSION_COOKIE_HTTPONLY
-        Refs #15618 and #20972.
+        Ensure that CookieStorage honors SESSION_COOKIE_DOMAIN.
+        Refs #15618.
         """
         # Test before the messages have been consumed
         storage = self.get_storage()
@@ -68,15 +69,13 @@ class CookieTest(BaseTests, TestCase):
         self.assertTrue('test' in response.cookies['messages'].value)
         self.assertEqual(response.cookies['messages']['domain'], '.example.com')
         self.assertEqual(response.cookies['messages']['expires'], '')
-        self.assertEqual(response.cookies['messages']['secure'], True)
-        self.assertEqual(response.cookies['messages']['httponly'], True)
 
-        # Test deletion of the cookie (storing with an empty value) after the messages have been consumed
+        # Test after the messages have been consumed
         storage = self.get_storage()
         response = self.get_response()
         storage.add(constants.INFO, 'test')
         for m in storage:
-            pass  # Iterate through the storage to simulate consumption of messages.
+            pass # Iterate through the storage to simulate consumption of messages.
         storage.update(response)
         self.assertEqual(response.cookies['messages'].value, '')
         self.assertEqual(response.cookies['messages']['domain'], '.example.com')
@@ -125,8 +124,8 @@ class CookieTest(BaseTests, TestCase):
         messages = [
             {
                 'message': Message(constants.INFO, 'Test message'),
-                'message_list': [Message(constants.INFO, 'message %s')
-                                 for x in range(5)] + [{'another-message':
+                'message_list': [Message(constants.INFO, 'message %s') \
+                                 for x in range(5)] + [{'another-message': \
                                  Message(constants.ERROR, 'error')}],
             },
             Message(constants.INFO, 'message %s'),
--- environment/lib/python2.7/site-packages/django/utils/safestring.py
+++ environment/lib/python2.7/site-packages/django/utils/safestring.py
@@ -7,17 +7,14 @@ be interpreted by the HTML engine (e.g. '<') into the appropriate entities.
 from django.utils.functional import curry, Promise
 from django.utils import six
 
-
 class EscapeData(object):
     pass
-
 
 class EscapeBytes(bytes, EscapeData):
     """
     A byte string that should be HTML-escaped when output.
     """
     pass
-
 
 class EscapeText(six.text_type, EscapeData):
     """
@@ -32,16 +29,8 @@ else:
     # backwards compatibility for Python 2
     EscapeUnicode = EscapeText
 
-
 class SafeData(object):
-    def __html__(self):
-        """
-        Returns the html representation of a string.
-
-        Allows interoperability with other template engines.
-        """
-        return self
-
+    pass
 
 class SafeBytes(bytes, SafeData):
     """
@@ -74,7 +63,6 @@ class SafeBytes(bytes, SafeData):
             return SafeText(data)
 
     decode = curry(_proxy_method, method=bytes.decode)
-
 
 class SafeText(six.text_type, SafeData):
     """
@@ -113,7 +101,6 @@ else:
     # backwards compatibility for Python 2
     SafeUnicode = SafeText
 
-
 def mark_safe(s):
     """
     Explicitly mark a string as safe for (HTML) output purposes. The returned
@@ -128,7 +115,6 @@ def mark_safe(s):
     if isinstance(s, (six.text_type, Promise)):
         return SafeText(s)
     return SafeString(str(s))
-
 
 def mark_for_escaping(s):
     """
@@ -145,3 +131,4 @@ def mark_for_escaping(s):
     if isinstance(s, (six.text_type, Promise)):
         return EscapeText(s)
     return EscapeBytes(bytes(s))
+
