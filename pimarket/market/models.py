a97f89817b6933489bb2964571116d71fca6f688
adwittumuluri <AdwitTumuluri@dhcp-ccc-438.eduroam.cornell.edu>
2014-09-26 22:52:12 -0400

    In the process of adding offers

--- pimarket/market/models.py
+++ pimarket/market/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Offer(models.Model):
+	item         = models.CharField(max_length=30)
+	buyer        = models.CharField(max_length=30)
+	seller       = models.CharField(max_length=30)
+	date_created = models.CharField(max_length=30)
+	date_sold    =  models.CharField(max_length=30)
