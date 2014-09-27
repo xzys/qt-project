e8a35af964fceaa4cc12e3bfe11945d3501e7604
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-09-27 09:15:26 -0400

    django version changse

--- pimarket/market/admin.py
+++ pimarket/market/admin.py
@@ -5,27 +5,34 @@ from market.models import UserProfile, Location, Offer, ItemGroup, Item, Textboo
 
 
 
-@admin.register(UserProfile)
+# @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
 	pass
 
-@admin.register(Location)
+# @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
 	pass
 	
-@admin.register(Offer)
+# @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
 	pass
 
-@admin.register(ItemGroup)
+# @admin.register(ItemGroup)
 class ItemGroupAdmin(admin.ModelAdmin):
 	pass
 
-@admin.register(Textbook)
+# @admin.register(Textbook)
 class TextbookAdmin(admin.ModelAdmin):
 	pass
 
-@admin.register(Ticket)
+# @admin.register()
 class TicketAdmin(admin.ModelAdmin):
 	pass
 
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(ItemGroup, ItemGroupAdmin)
+admin.site.register(Textbook, TextbookAdmin)
+admin.site.register(Ticket, TicketAdmin)
