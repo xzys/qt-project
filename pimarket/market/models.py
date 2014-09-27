cc38f89f77a4e69df8834ba110573cf2c123819c
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-09-26 22:50:11 -0400

    some models stuff

--- pimarket/market/models.py
+++ pimarket/market/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+	# reference to django User
+
+	user = models.OneToOneField(User, primary_key=true)
+
+
+# represents any item to sell
+class Item(models.Model):
+	user1 = models.ForeignKey(User, related_name='r+')
+	user2 = models.ForeignKey(User, related_name='b+')
+
+	started_date = models.DateTImeField()
+
+
+	class Meta:
+		abstract = True
