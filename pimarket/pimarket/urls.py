b18adb1fd72f0c0f278f749923db72911ebee696
Genki Marshall <genkimarshall@gmail.com>
2014-09-27 10:53:39 -0400

    bringing dev to modern times

--- pimarket/pimarket/urls.py
+++ pimarket/pimarket/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pimarket.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^splash/', 'market.views.home',name="home"),
+    url(r'^login/', 'market.views.login', name="login"),
+    url(r'^', 'market.views.home',name="home"),
     url(r'^admin/', include(admin.site.urls)),
 )
+
