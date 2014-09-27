e63b6cfe6fe9ec696d01652623dcb13c5597cb46
Genki Marshall <genkimarshall@gmail.com>
2014-09-27 15:49:40 -0400

    Getting textbooks in textbooks

--- wsgi/openshift/market/static/js/requests.js
+++ wsgi/openshift/market/static/js/requests.js
@@ -1,17 +1,8 @@
 $(document).ready(function(){
-	var url = "localhost:8000/api/getjson";
-	var settings = {
-		data: 'category=A',
-	};
-	// $.ajax(url, settings).done(function(result) {
-	// 	var result = JSON.parse(result);
-	// 	console.log(result);
-	// });
 	$.get(
-		"localhost:8000/api/getjson", 
+		'http://localhost:8000/api/getjson', 
 		{ category: 'A' },
 	    function(result) {
-			var result = JSON.parse(result);
 			console.log(result);
 		}
 	);
