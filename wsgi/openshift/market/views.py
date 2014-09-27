558b750251235009b96d10e89a6126f36e2b18d2
Genki Marshall <genkimarshall@gmail.com>
2014-09-27 12:44:47 -0400

    Fix views.py

--- wsgi/openshift/market/views.py
+++ wsgi/openshift/market/views.py
@@ -4,18 +4,10 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 
 def home(request):
-<<<<<<< HEAD
 	print "HOME SWETT"
-	return render_to_response("market/login.html")
-
-def login(request):
-	print "LOGIN INING"
-=======
-	# print "HOME SWETT HOME"
 	return render_to_response("market/index.html")
 
 def login(request):
-	# print "LOGIN INING"
->>>>>>> e0e7b172d3e2d08fa7ef476f4f5b0fec936106f3
+	print "LOGIN INING"
 	# return render_to_response('home/home.html')
 	return render_to_response("market/login.html")
