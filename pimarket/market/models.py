e4a789b81816e163d64f6cb0361d54464ac60003
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-09-27 00:01:10 -0400

    models.py formatting

--- pimarket/market/models.py
+++ pimarket/market/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
 	# simple reference to django User
-	user = models.OneToOneField(User, primary_key=true)
+	user 		= models.OneToOneField(User, primary_key=true)
 
 	# Preferences
-	groups = models.ManyToManyField('ItemGroup',
+	groups 		= models.ManyToManyField('ItemGroup',
 		related_name = 'a+',
 		null = True, blank = False)
 
-	locations = models.ManyToManyField('Location',
+	locations 	= models.ManyToManyField('Location',
 		related_name = 'b+')
 		null = True, blank = False)
 	
@@ -22,26 +22,24 @@ class UserProfile(models.Model):
 
 
 class Location(model.Model):
-	name = models.CharField(max_length=100)
-	# one or the other
-	lattitude = models.FloatField()
-	longitude = models.FloatField() 
+	name 		= models.CharField(max_length=100)
+	lattitude 	= models.FloatField()
+	longitude 	= models.FloatField() 
 
 	def __unicode__(self):
 		return str(self.longitude) + ', ' + str(self.lattitude)
 
 ############### ITEMS ######################
 class ItemGroup(model.Model):
-	name = models.CharField(max_length=100)
+	name 		= models.CharField(max_length=100)
 
 	def __unicode__(self):
 		return self.name
 
 # represents any item to sell
 class Item(models.Model):
-	seller = models.ForeignKey(User, related_name='r+')
-	# price is deciman 2 units
-	price = models.DecimalField(max_digits=6, decimal_places=2)
+	seller		= models.ForeignKey(User, related_name='r+')
+	price		= models.DecimalField(max_digits=6, decimal_places=2)
 
 	class Meta:
 		abstract = True
@@ -56,18 +54,20 @@ class Textbook(Item):
         (4, 'New'),
     )
 
-	author = models.CharField(max_length=100)
-	ISBN = models.CharField(max_length=100)
-	title = models.CharField(max_length=100)
-	condition = models.CharField(max_length=100
+	author 		= models.CharField(max_length=100)
+	ISBN 		= models.CharField(max_length=100)
+	title 		= models.CharField(max_length=100)
+
+	# chooses from above
+	condition 	= models.CharField(max_length=100
 		choices = CONDITION_CHOICES)
 
     def __unicode__(self):
         return self.title, 'by:', self.author
 
 class Ticket(Item):
-	event = models.CharField(max_length=100)
-	date = models.DateTimeField(null=True, blank=True)
+	event 		= models.CharField(max_length=100)
+	date 		= models.DateTimeField(null=True, blank=True)
 
     def __unicode__(self):
         return self.event
