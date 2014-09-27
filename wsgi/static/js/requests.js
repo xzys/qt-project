75f69da9e11d19fb219708d545d76a7105709550
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-09-27 17:37:28 -0400

    checkboxes check.

--- wsgi/static/js/requests.js
+++ wsgi/static/js/requests.js
@@ -1,17 +1,32 @@
-$(document).ready(function(){
+function requestListings (category) {
 	var listings = $("#listings");
 	$.get(
 		'http://localhost:8000/api/getjson', 
-		{ category: 'A' },
+		{ category: category },
 	    function(result) {
 	    	var listingsResponse = result.listings;
 			for (i=0;i<listingsResponse.length;i++) {
-				console.log(i);
 				var fields = listingsResponse[i].fields;
-				listings.append('<div class="panel panel-default"><div class="panel-body">'+
-					fields.title + '<br>' + fields.isbn + '<br>' + fields.condition + 
-					'</div></div></div>');
+				var toAppend = '<div class="panel panel-default"><div class="panel-body">';
+				if (category == 'A') {
+					toAppend = toAppend + fields.title + '<br>' + fields.isbn + '<br>' + fields.condition;
+				} else {
+					toAppend = toAppend + fields.event + '<br>' + fields.date;
+				}
+				toAppend + '</div></div></div>';
+				listings.append(toAppend);
 			}
 		}
 	);
+}
+
+$(document).ready(function() {
+	var pathname = window.location.pathname;
+	if (pathname.indexOf("textbooks") != -1) {
+		$("#textbooks").addClass("activeCategory");
+		requestListings('A');
+	} else if (pathname.indexOf("tickets") != -1) {
+		$("#tickets").addClass("activeCategory");
+		requestListings('B');
+	}
 });
