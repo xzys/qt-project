184c63955c9ced64dd40880b21cf45575dcaf21a
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2014-09-27 22:18:04 -0400

    other

--- wsgi/openshift/market/views.py
+++ wsgi/openshift/market/views.py
@@ -236,7 +236,7 @@ def api_request(request, action):
 		
 		jsondata = json.dumps({
 			'listings' : results['listings'],
-			'others' : results['other'],
+			'other' : results['other'],
 			}, default=dthandler)
 		return HttpResponse(jsondata, content_type='application/json')
 
